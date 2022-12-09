f6b264abde58d163d303b9f0b134714d25b8f563
Daniel Afanador <danielthemisfits@gmail.com>
2022-12-09 09:55:42 -0300

    Included cases in which the app name could be in English, but using special characters

--- app-names-english.py
+++ app-names-english.py
@@ -1,15 +1,21 @@
 def nonEnglish (string):
     
     counter = 0 
+    nonEnglishCounter = 0
     
     for i in string:    
         
         if ord(i) <= 127:
             counter = counter + 1
             
-            if counter == len (string):
-                print ('This App is in English.')
-            
         elif ord(i)>= 127:
-            print ('This App is not in English or may have unknown characters.')
-            break
+            nonEnglishCounter = nonEnglishCounter + 1
+                    
+    if counter == len (string):
+        print ('The name of this App is in English.')
+            
+    elif nonEnglishCounter <= 2:
+        print ('The name of this App is in English, but may contain non-English characters or emojis.')
+            
+    else:
+        print ('This App is not in English or may have unknown characters.')
