22722f669c45765b74389e7cbaef7e5b724f9548
Daniel Afanador <danielthemisfits@gmail.com>
2022-12-12 20:00:58 -0300

    We have now 2 solutions for the same problem

--- app-names-english.py
+++ app-names-english.py
@@ -1,3 +1,5 @@
+##SOLUTION 1##
+
 apps_in_english = []
 apps_w_special_characters = []
 apps_not_in_english = []
@@ -29,4 +31,27 @@ for app in list_of_csv[1:]:
         
 #print (apps_in_english)
 #print (apps_w_special_characters)
+#print (apps_not_in_english)
+
+###################################################
+
+##SOLUTION 2##
+
+apps_in_english = []
+apps_not_in_english = []
+
+def is_english(app_name):
+    return app_name.isascii()
+    
+for app in list_of_csv[1:]:  
+    
+    name = app [0]
+    
+    if is_english(name) == True:
+        apps_in_english.append(name)
+
+    else:
+        apps_not_in_english.append(name)
+        
+#print (apps_in_english)
 #print (apps_not_in_english)
